e995dce3058fe02f32ced0c349c72771c22e5474
Maowss <Maowss@gmail.com>
2019-08-21 22:07:52 -0300

    V2

--- app/src/main/java/br/com/etecia/myapplication/JanelaDois_Activity.java
+++ app/src/main/java/br/com/etecia/myapplication/JanelaDois_Activity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class JanelaDois_Activity extends AppCompatActivity {
 
@@ -14,6 +16,16 @@ public class JanelaDois_Activity extends AppCompatActivity {
         setContentView(R.layout.janela_dois_layout);
 
         btnvoltarjanela1 = (Button) findViewById(R.id. btnVoltarJanela1);
+        btnvoltarjanela1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Clique aqui",
+                        Toast.LENGTH_SHORT). show();
+
+                
+            }
+        });
 
 
 
